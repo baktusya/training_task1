593ca2a76b5f1efad704b6af9b949b8b3dffcb4f
baktusya <baktusya@mail.ru>
2024-09-05 13:34:48 +0300

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,17 @@
+#1st program
+print(9**0.5*5)
+
+#2st program
+print(9.99>9.98 and 1000!=1000.1)
+
+#3st program
+print(2*2+2)
+print(2*(2+2))
+print(2*2+2==2*(2+2))
+
+#4st program
+step1 = '123.456'
+step2 = float(step1)
+step3 = step2*10
+step4 = int(step3)
+print(step4%10)
